e37f75ec678ef4cdfb4ecbb0f59ffa628fd5b0cd
Gabriel <gabriel.gsm2017@gmail.com>
2021-12-16 18:57:39 -0300

    finalização

---
+++ script.js
@@ -0,0 +1,40 @@
+function exibirLetras(value) {
+    const numerosLetras = document.getElementById('numerosLetras');
+
+    if(value.checked === true){
+        numerosLetras.style.display = "flex";
+    }else{
+        numerosLetras.style.display = "none";
+    }
+
+}
+
+function exibirPalavras(value) {
+    const numerosPalavras = document.getElementById('numerosPalavras');
+
+    if(value.checked === true){
+        numerosPalavras.style.display = "flex";
+    }else{
+        numerosPalavras.style.display = "none";
+    }
+
+}
+
+function contarLetrasPalavras(area) {
+    const numerosLetras = document.getElementById('numerosLetras');
+    const numerosPalavras = document.getElementById('numerosPalavras');
+
+    const resultadoLetras = area.value.length;
+    let resultadoPalavras = area.value.split(' ').length;
+    
+    if(area.value === ""){
+        resultadoPalavras = 0;
+    }
+
+    
+    numerosLetras.innerHTML = `${resultadoLetras} letras`; 
+    numerosPalavras.innerHTML = `${resultadoPalavras} palavras`;
+
+}
+
+
